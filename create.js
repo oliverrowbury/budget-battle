8a687fe0bc6833b9a6d97361fa8427637cfefa1f
oliverrowbury <oliverrowbury@gmail.com>
2026-08-19 22:45:40 +0100

    Add functionality to preselect category button

--- create.js
+++ create.js
@@ -35,3 +35,8 @@ function updateStartButton() {
 }
 
 updateStartButton();
+const preselectedKey = new URLSearchParams(window.location.search).get("category");
+if (preselectedKey) {
+  const preselectedButton = document.querySelector(`.category-button[data-target="${preselectedKey}"]`);
+  if (preselectedButton) preselectedButton.click();
+}
